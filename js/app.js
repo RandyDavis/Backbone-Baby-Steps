3601fda131efb291e87dfb99fa1f522dcccd1ac9
Randy Davis <randy_davis@outlook.com>
2015-02-28 18:26:56 -0600

    add BookView View

--- js/app.js
+++ js/app.js
@@ -8,4 +8,17 @@
       keywords: "JavaScript Programming"
     }
   });
+
+  var BookView = Backbone.View.extend({
+    tagName: "div",
+    className: "bookContainer",
+    template: $("#bookTemplate").html(),
+
+    render: function() {
+      var tmpl = _.template(this.template);  // tmpl is a function that takes a JSON object and returns html
+
+      this.$el.html(tmpl(this.model.toJSON()));  // this.el is what we define in tagName.  Use $el to get access to jQuery html() function
+      return this;
+    }
+  });
 }) (jQuery);
