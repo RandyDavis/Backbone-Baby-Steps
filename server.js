89ccfd74ddbbf28eb17b6b26f8af6c6a96168ba9
Randy Davis <randy_davis@outlook.com>
2015-03-05 21:34:57 -0600

    update POST and PUT to include keywords now

--- server.js
+++ server.js
@@ -60,7 +60,8 @@ app.post('/api/books', function(req, res) {
   var book = new BookModel({
     title:req.body.title,
     author:req.body.author,
-    releaseDate:req.body.releaseDate
+    releaseDate:req.body.releaseDate,
+    keywords: req.body.keywords
   });
   book.save(function(err) {
     if (!err) {
@@ -90,6 +91,8 @@ app.put('/api/books/:id', function(req, res) {
     book.title = req.body.title;
     book.author = req.body.author;
     book.releaseDate = req.body.releaseDate;
+    book.keywords = req.body.keywords;
+
     return book.save(function(err) {
       if(!err) {
         console.log("book updated");
